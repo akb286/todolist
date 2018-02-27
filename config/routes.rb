6b45e23dd50e7a2f681d336dcb28b46fb03b4b70
Alicia Baker <aliciabaker314@gmail.com>
2018-02-26 17:14:06 -0700

    it looks nice

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'tasks#home'
+
+  get '/about', to: 'tasks#about'
+
+  resources :tasks
 end
